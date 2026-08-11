7907cb8ad4c63d4fcef10f8b8dae0c813ad21ab2
T-Luxshan <luckybraveboys@gmail.com>
2026-08-11 13:18:37 +0530

    init repo

--- backend/src/main/java/com/luxshan/linkshort/exception/LinkNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.luxshan.linkshort.exception;
-
-public class LinkNotFoundException extends RuntimeException {
-
-    public LinkNotFoundException(String shortCode) {
-        super("Short link not found: " + shortCode);
-    }
-}
